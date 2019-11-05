2e52b0e3bdb4797ab8abb8713c3fbc49d9ef6ad1
Yuval Shavit <yankee.sierra@gmail.com>
2019-11-05 01:32:41 -0500

    Initial Commit

---
+++ wtfdid/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  wtfdid
+//
+//  Created by Yuval Shavit on 11/5/19.
+//  Copyright © 2019 Yuval Shavit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
