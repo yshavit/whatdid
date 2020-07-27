b697be8094c99f9848de3db3dec4d34f59d39333
Yuval Shavit <yankee.sierra@gmail.com>
2020-07-26 20:07:59 -0400

    better code for version handling
    We use `buildscripts/set_plist_build.sh` to get the abbreviated git
    hash, which we then convert to decimal and put as the patch number. To
    make this work, the public version has to always have the major and
    minor versions, but not patch number. Then, if the git space is dirty,
    we append `0xFFFF` to that hash (before we convert it to decimal).
    
    In Version.swift, we reverse the process: we parse the version string's
    components, and then treat the build number as a String (not decimal
    number), which is the abbreviated hash, potentially with `"-dirty"` as a
    suffix.

--- buildscripts/set_plist_build.sh
+++ buildscripts/set_plist_build.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-sha="$(git log -n 1 '--pretty=format:%h')"
-
-git_dirty=''
+buildnum="$(git log -n 1 '--pretty=format:%h')"
 if [[ $(git status -s | wc -c) -ne 0 ]]; then 
-  git_dirty='-dirty'
+  # See Version.swift for how we parse this
+  buildnum="${buildnum}FFFF"
 fi
-
-build_string="${sha}${git_dirty}"
+buildnum="$((16#$buildnum))"
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_string" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $build_string" "$plist"
+    short_string="$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$plist")"
+    if ! grep -q '^[0-9]\+\.[0-9]\+$' <<< "$short_string" ; then
+      self_without_prefix="${0#"$SRCROOT"}"
+      echo "Error!!"
+      echo "Error!!"
+      echo "Error!! .$self_without_prefix:"
+      echo "Error!! Version number must have exactly two parts ('xx.yy', not 'xx' or 'xx.yy.zz')"
+      echo "Error!! Is: $short_string"
+      echo "Error!!"
+      echo "Error!!"
+      exit 1
+    fi
+    full_string="${short_string}.$buildnum"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $full_string" "$plist"
   fi
 done
