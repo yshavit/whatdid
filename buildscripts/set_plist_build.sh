ba5b4899eb6bc1adf65e0613889d41ff02fa034a
Yuval Shavit <yankee.sierra@gmail.com>
2020-07-28 01:11:18 -0400

    only edit files as needed

--- buildscripts/set_plist_build.sh
+++ buildscripts/set_plist_build.sh
@@ -11,8 +11,14 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 for plist in "$info_plist" "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
     echo "Found plist:  $plist"
-    /usr/libexec/PlistBuddy -c "Delete :ComYuvalShavitWtfdidVersion" "$plist" || true
-    /usr/libexec/PlistBuddy -c "Add :ComYuvalShavitWtfdidVersion string $sha" "$plist"
+    old_sha=$(/usr/libexec/PlistBuddy -c "Print :ComYuvalShavitWtfdidVersion" "$plist" || echo '<none>')
+    if [[ "$old_sha" != "$sha" ]] ; then
+      echo "Updating sha to $sha (was: $old_sha)"
+      /usr/libexec/PlistBuddy -c "Delete :ComYuvalShavitWtfdidVersion" "$plist" || true
+      /usr/libexec/PlistBuddy -c "Add :ComYuvalShavitWtfdidVersion string $sha" "$plist"
+    else
+      echo "Found old sha ($sha), leaving untouched."
+    fi
   else
     echo "Missing plist: $plist"
   fi
