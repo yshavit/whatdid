79d7f19d00d4b168ca1c65ba1605a2f00f81fc72
Yuval Shavit <yankee.sierra@gmail.com>
2020-08-01 02:03:34 -0400

    checkpoint 01: coding up projects report
    This step is just some groundwork: setting view identifiers, and
    removing some unsatisfiable (and uneeded) constraints. We don't need
    vertical spacing constraints, because the vstack automatically provides
    some.

--- whatdid/DayEndReportController.swift
+++ whatdid/DayEndReportController.swift
@@ -9,4 +9,25 @@ class DayEndReportController: NSViewController {
         // Do view setup here.
     }
     
+    @IBOutlet weak var projectsContainer: NSStackView!
+    
+    override func viewWillAppear() {
+        projectsContainer.subviews.forEach {$0.removeFromSuperview()}
+        _ = Model.group(entries: getEntries()) // TODO read from Model
+    }
+    
+    private func getEntries() -> [Model.FlatEntry] {
+        func d(_ hh: Int, _ mm: Int) -> Date {
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss-4:00"
+            return dateFormatter.date(from: String(format: "2020-07-31T%02d:%02d:00-4:00", hh, mm))!
+        }
+        return [
+            .init(from: d(10, 00), to: d(10, 15), project: "Project1", task: "Task 1", notes: "entry 1"),
+            .init(from: d(10, 15), to: d(10, 30), project: "Project1", task: "Task 1", notes: "entry 2"),
+            .init(from: d(10, 30), to: d(10, 45), project: "Project1", task: "Task 2", notes: "entry 3"),
+            .init(from: d(10, 45), to: d(11, 00), project: "Project2", task: "Task 1", notes: "entry 4"),
+        ].shuffled() // to make it interesting :)
+    }
 }
