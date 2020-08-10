a21db41e3c553cb43bd27fad8c24b3b33213edd1
Yuval Shavit <yankee.sierra@gmail.com>
2020-08-10 01:34:30 -0400

    add helper to handle opening/closing the window
    There are a few states we need to worry about:
    
    - opening the window when it's closed (easy)
    - opening a scheduled PTN while the PTN is already open
    - ... while the daily report is already open
    - opening the daily report while the daily report is already open
    - ... while the PTN is open
    
    Note that we don't need to worry about manually opening the window while
    it's already opened; the UI won't allow that.
    
    I think we can sum all of the above with a few easy rules:
    
    - If window _A_ is open due to a schedule, or if someone tries to
      schedule another _A_ while _A_ is already open (for any reason), then
      schedule another _A_ when the window closes.
    - If window _A_ tries to open when window _B_ is already open, then once
      _B_ is open, (1) schedule _B_ if needed by the above rule, and then
      immediately open _A_; and when _A_ later closes, schedule another _A_
      if needed by the above rule.
    
    I think this will also generalize to other window types (like the daily
    start), once we have those.
    
    This is the generalization for the open logic that we wanted in #71.
    I'll apply it to the actual UI in the next commit.

---
+++ whatdid/util/OpenCloseHelper.swift
@@ -0,0 +1,77 @@
+// whatdid?
+
+import Cocoa
+
+class OpenCloseHelper<T: Hashable & Comparable> {
+    
+    private let opener: (T) -> Void
+    private let scheduler: (T) -> Void
+    
+    var openItem: T? = nil
+    private var pendingOpens = [T: OpenReason]()
+    private var rescheduleOnClose = false
+    private var isSnoozed = false
+    
+    init(onOpen: @escaping (T) -> Void, onSchedule: @escaping (T) -> Void) {
+        self.opener = onOpen
+        self.scheduler = onSchedule
+    }
+    
+    func open(_ item: T, reason: OpenReason) {
+        if openItem == nil {
+            if (reason == .scheduled) && isSnoozed {
+                pendingOpens[item] = reason
+            } else {
+                openItem = item
+                opener(item)
+                rescheduleOnClose = reason == .scheduled
+            }
+        } else if reason == .scheduled {
+            if openItem == item {
+                rescheduleOnClose = true
+            } else {
+                pendingOpens[item] = reason
+            }
+        } else {
+            // If we're already open, we shouldn't be able to get a manual open; the UI should prevent that.
+            // We should check that condition (as we just did) to be safe and to let us write comprehensive unit tests,
+            // but really it's just a weird corner case.
+            NSLog("WARN Tried to open \(item) manually while \(openItem!) was already open")
+        }
+    }
+    
+    func snooze() {
+        isSnoozed = true
+    }
+    
+    func unSnooze() {
+        isSnoozed = false
+        pullFromPending()
+    }
+    
+    func didClose() {
+        guard openItem != nil else {
+            return
+        }
+        if rescheduleOnClose {
+            let item = openItem!
+            rescheduleOnClose = false
+            openItem = nil
+            // Schedule even if we're snoozed; if we're still snoozed when the schedule hits, then
+            // it'll enqueue itself
+            scheduler(item)
+        }
+        openItem = nil
+        if !isSnoozed {
+            pullFromPending()
+        }
+    }
+    
+    private func pullFromPending() {
+        if let deferredOpenKey = pendingOpens.keys.sorted().first {
+            rescheduleOnClose = pendingOpens.removeValue(forKey: deferredOpenKey)! == .scheduled
+            openItem = deferredOpenKey
+            opener(deferredOpenKey)
+        }
+    }
+}
