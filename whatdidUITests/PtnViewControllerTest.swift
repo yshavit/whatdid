yshavit/whatdid

commit e9912a4dbf9730963c2486c81b298f7e0d30a80f

UI tweaks/fixes
- start up in agent mode, not background-only (maybe this will fix
  [this test failure][1]?)
- fit the PTN window when you open it

[1]: https://github.com/yshavit/whatdid/pull/92/checks?check_run_id=1046447339

whatdid/MainMenu.swift
@@ -85,7 +85,7 @@ class MainMenu: NSWindowController, NSWindowDelegate, NSMenuDelegate {
             window?.title = "What are you working on?"
         }
         
-        window?.layoutIfNeeded()
+        window!.setContentSize(window!.contentViewController!.view.fittingSize)
         if let mainFrame = NSScreen.main?.visibleFrame, let button = statusItem.button {
             let buttonBoundsAbsolute = button.window?.convertToScreen(button.bounds)
             var pos = NSPoint(

whatdid/AppDelegate.swift
@@ -32,10 +32,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         uiTestWindow = UiTestWindow()
         uiTestWindow.show()
         NSApp.setActivationPolicy(.regular) // UI tests can time out on launch() without this
-        #else
-        // Our Info.plist starts us off as background. Now that we're started, become an accessory app.
-        // This approach lets us start the app deactivated.
-        NSApp.setActivationPolicy(.accessory)
         #endif
         
         AppDelegate.DEBUG_DATE_FORMATTER.timeZone = DefaultScheduler.instance.timeZone

whatdid/Info.plist
@@ -22,8 +22,6 @@
 	<string>1</string>
 	<key>LSApplicationCategoryType</key>
 	<string>public.app-category.productivity</string>
-	<key>LSBackgroundOnly</key>
-	<true/>
 	<key>LSMinimumSystemVersion</key>
 	<string>$(MACOSX_DEPLOYMENT_TARGET)</string>
 	<key>NSHumanReadableCopyright</key>
@@ -33,6 +31,8 @@
 	<key>NSPrincipalClass</key>
 	<string>NSApplication</string>
 	<key>NSSupportsAutomaticTermination</key>
+	<true/>
+	<key>LSUIElement</key>
 	<true/>
 	<key>NSSupportsSuddenTermination</key>
 	<true/>

commit 073b27a11ad2ee7939a8f1766f68103635dbae6b

fix: daily report wasn't resizing vertically
Add a UI test for it. This resolves #97, which actually was _not_ caused
by e9912a4 (as I first suspected). Instead, it was caused by the new
scheduler from a bit ago.

whatdid/DayEndReportController.swift
@@ -86,12 +86,17 @@ class DayEndReportController: NSViewController {
             projectVStack.widthAnchor.constraint(equalTo: projectsContainer.widthAnchor, constant: -2).isActive = true
             projectVStack.leadingAnchor.constraint(equalTo: projectsContainer.leadingAnchor).isActive = true
             
-            let projectHeader = ExpandableProgressBar(addTo: projectVStack, labeled: project.name, withDuration: project.totalTime, outOf: allProjectsTotalTime)
-            
+            let projectHeader = ExpandableProgressBar(
+                addTo: projectVStack,
+                label: project.name,
+                accessibilityLabelScope: "Project",
+                withDuration: project.totalTime,
+                outOf: allProjectsTotalTime)
             // Tasks box
             let tasksBox = NSBox()
             projectVStack.addArrangedSubview(tasksBox)
-            tasksBox.title = "Tasks for \(project.name)"
+            tasksBox.setAccessibilityLabel("Tasks for \(project.name)")
+            tasksBox.title = tasksBox.accessibilityLabel()!
             tasksBox.titlePosition = .noTitle
             tasksBox.leadingAnchor.constraint(equalTo: projectVStack.leadingAnchor, constant: 3).isActive = true
             tasksBox.trailingAnchor.constraint(equalTo: projectVStack.trailingAnchor, constant: -3).isActive = true
@@ -112,7 +117,12 @@ class DayEndReportController: NSViewController {
             
             var previousDetailsBottomAnchor : NSLayoutYAxisAnchor?
             project.forEach {task in
-                let taskHeader = ExpandableProgressBar(addTo: tasksStack, labeled: task.name, withDuration: task.totalTime, outOf: allProjectsTotalTime)
+                let taskHeader = ExpandableProgressBar(
+                    addTo: tasksStack,
+                    label: task.name,
+                    accessibilityLabelScope: "Task",
+                    withDuration: task.totalTime,
+                    outOf: allProjectsTotalTime)
                 taskHeader.progressBar.leadingAnchor.constraint(equalTo: projectHeader.progressBar.leadingAnchor).isActive = true
                 taskHeader.progressBar.trailingAnchor.constraint(equalTo: projectHeader.progressBar.trailingAnchor).isActive = true
                 previousDetailsBottomAnchor?.constraint(equalTo: taskHeader.topView.topAnchor, constant: -5).isActive = true
@@ -134,6 +144,7 @@ class DayEndReportController: NSViewController {
                 let taskDescriptions = details.trimmingCharacters(in: .newlines)
                 let taskDetailsView = NSTextField(labelWithString: taskDescriptions)
                 tasksStack.addArrangedSubview(taskDetailsView)
+                taskDetailsView.setAccessibilityLabel("Details for \(task.name)")
                 taskDetailsView.font = NSFont.systemFont(ofSize: NSFont.smallSystemFontSize)
                 taskDetailsView.leadingAnchor.constraint(equalTo: taskHeader.progressBar.leadingAnchor).isActive = true
                 previousDetailsBottomAnchor = taskDetailsView.bottomAnchor
@@ -198,7 +209,7 @@ class DayEndReportController: NSViewController {
         let disclosure: ButtonWithClosure
         let progressBar: NSProgressIndicator
         
-        init(addTo enclosing: NSStackView, labeled label: String, withDuration duration: TimeInterval, outOf: TimeInterval) {
+        init(addTo enclosing: NSStackView, label: String, accessibilityLabelScope scope: String, withDuration duration: TimeInterval, outOf: TimeInterval) {
             let labelStack = NSStackView()
             enclosing.addArrangedSubview(labelStack)
             labelStack.orientation = .horizontal
@@ -207,9 +218,11 @@ class DayEndReportController: NSViewController {
             let projectLabel = NSTextField(labelWithString: label)
             projectLabel.font = NSFont.systemFont(ofSize: NSFont.systemFontSize)
             labelStack.addView(projectLabel, in: .leading)
+            projectLabel.setAccessibilityLabel("\(scope) \"\(label)\"")
             let durationLabel = NSTextField(labelWithString: TimeUtil.daysHoursMinutes(for: duration))
             durationLabel.font = NSFont.systemFont(ofSize: NSFont.smallSystemFontSize)
             labelStack.addView(durationLabel, in: .trailing)
+            durationLabel.setAccessibilityLabel("\(scope) time for \"\(label)\"")
             
             let headerHStack = NSStackView()
             enclosing.addArrangedSubview(headerHStack)
@@ -221,6 +234,7 @@ class DayEndReportController: NSViewController {
             disclosure = createDisclosure(state: .off)
             headerHStack.addArrangedSubview(disclosure)
             disclosure.leadingAnchor.constraint(equalTo: headerHStack.leadingAnchor).isActive = true
+            disclosure.setAccessibilityLabel("\(scope) details toggle for \"\(label)\"")
             
             // progress bar
             progressBar = NSProgressIndicator()
@@ -231,6 +245,7 @@ class DayEndReportController: NSViewController {
             progressBar.doubleValue = duration
             progressBar.trailingAnchor.constraint(lessThanOrEqualTo: headerHStack.trailingAnchor).isActive = true
             progressBar.trailingAnchor.constraint(equalTo: labelStack.trailingAnchor).isActive = true
+            progressBar.setAccessibilityLabel("\(scope) activity indicator for \"\(label)\"")
             
             topView = labelStack
         }

whatdid/MainMenu.swift
@@ -58,6 +58,7 @@ class MainMenu: NSWindowController, NSWindowDelegate, NSMenuDelegate {
             onOpen: {contents in
                 DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()) {
                     self.open(contents)
+                    self.focus()
                 }
             },
             onSchedule: self.schedule)
@@ -71,7 +72,6 @@ class MainMenu: NSWindowController, NSWindowDelegate, NSMenuDelegate {
                 ? WindowContents.dailyEnd
                 : WindowContents.ptn
             opener.open(showWhat, reason: .manual)
-            focus()
         }
     }
     

whatdidUITests/PtnViewControllerTest.swift
@@ -39,7 +39,7 @@ class PtnViewControllerTest: XCTestCase {
             tcombo: AutocompleteFieldHelper(element: ptn.comboBoxes["tcombo"]))
     }
     
-    func clickStatusMenu(){
+    func clickStatusMenu(with flags: CGEventFlags = []){
         // In headless mode (or whatever GH actions uses), I can't just use the XCUIElement's `click()`
         // when the app is in the background. Instead, I fetched the status item's location during setUp, and
         // now directly post the click events to it.
@@ -47,6 +47,7 @@ class PtnViewControllerTest: XCTestCase {
             let src = CGEventSource(stateID: CGEventSourceStateID.hidSystemState)
             for eventType in [CGEventType.leftMouseDown, CGEventType.leftMouseUp] {
                 let event = CGEvent(mouseEventSource: src, mouseType: eventType, mouseCursorPosition: statusItemPoint, mouseButton: .left)
+                event?.flags = flags
                 event?.post(tap: CGEventTapLocation.cghidEventTap)
                 usleep(250000)
             }
@@ -142,7 +143,6 @@ class PtnViewControllerTest: XCTestCase {
         }
     }
     
-    // TODO: also test with just typing (not downarrow)
     func testAutoComplete() {
         let ptn = openPtn()
         group("initalize the data") {
@@ -165,6 +165,46 @@ class PtnViewControllerTest: XCTestCase {
             pcombo.typeKey(.downArrow)
             pcombo.typeText("\r")
             XCTAssertEqual("whodid", pcombo.stringValue)
+        }
+    }
+    
+    func testDailyReportPopup() {
+        group("Initalize the data") {
+            let ptn = openPtn()
+            let entriesTextField  = ptn.window.textFields["uihook_flatentryjson"]
+            let entries = EntriesBuilder()
+                .add(project: "project a", task: "task 1", notes: "first thing", minutes: 10)
+                .add(project: "project a", task: "task 2", notes: "sidetrack", minutes: 15)
+                .add(project: "project a", task: "task 1", notes: "back to first", minutes: 5)
+                .add(project: "project b", task: "task same", notes: "fizz", minutes: 5)
+                .add(project: "project c", task: "task same", notes: "fuzz", minutes: 10)
+                .serialize()
+            entriesTextField.deleteText(andReplaceWith: entries)
+            entriesTextField.typeKey(.enter)
+            clickStatusMenu()
+        }
+        let dailyReport = app.windows["Here's what you've been doing"]
+        group("Open daily report") {
+            clickStatusMenu(with: .maskAlternate)
+            XCTAssertTrue(dailyReport.isVisible)
+        }
+        group("Verify projects exist") {
+            for project in ["project a", "project b", "project c"] {
+                group(project) {
+                    for (description, e) in HierarchicalEntryLevel(ancestor: dailyReport, scope: "Project", label: project).allElements {
+                        XCTAssertTrue(e.exists, "\"\(project)\" \(description)")
+                    }
+                }
+            }
+        }
+        group("Verify projects visible") {
+            for project in ["project a", "project b", "project c"] {
+                group(project) {
+                    for (description, e) in HierarchicalEntryLevel(ancestor: dailyReport, scope: "Project", label: project).allElements {
+                        XCTAssertTrue(e.isHittable, "\"\(project)\" \(description)")
+                    }
+                }
+            }
         }
     }
     
@@ -327,8 +367,60 @@ class PtnViewControllerTest: XCTestCase {
         return FlatEntry(from: from, to: to, project: project, task: task, notes: notes)
     }
     
-    func t(_ timeDelta: TimeInterval) -> Date {
+    class func t(_ timeDelta: TimeInterval) -> Date {
         return PtnViewControllerTest.SOME_TIME.addingTimeInterval(timeDelta)
+    }
+    
+    func t(_ timeDelta: TimeInterval) -> Date {
+        return PtnViewControllerTest.t(timeDelta)
+    }
+    
+    class EntriesBuilder {
+        private var entries = [(p: String, t: String, n: String, duration: TimeInterval)]()
+        
+        func add(project: String, task: String, notes: String, minutes: Double) -> EntriesBuilder {
+            entries.append((p: project, t: task, n: notes, duration: minutes * 60.0))
+            return self
+        }
+        
+        func serialize(now: Date? = nil) -> String {
+            let totalInterval = entries.map({$0.duration}).reduce(0, +)
+            var startTime = t(-totalInterval)
+            var flatEntries = [FlatEntry]()
+            for e in entries {
+                let from = startTime
+                let to = startTime.addingTimeInterval(e.duration)
+                flatEntries.append(FlatEntry(from: from, to: to, project: e.p, task: e.t, notes: e.n))
+                startTime = to
+            }
+            return FlatEntry.serialize(flatEntries)
+        }
+    }
+    
+    struct HierarchicalEntryLevel {
+        let ancestor: XCUIElement
+        let scope: String
+        let label: String
+        
+        var headerLabel: XCUIElement {
+            ancestor.staticTexts["\(scope) \"\(label)\""].firstMatch
+        }
+        
+        var durationLabel: XCUIElement {
+            ancestor.staticTexts["\(scope) time for \"\(label)\""].firstMatch
+        }
+        
+        var disclosure: XCUIElement {
+            ancestor.disclosureTriangles["\(scope) details toggle for \"\(label)\""]
+        }
+        
+        var indicatorBar: XCUIElement {
+            ancestor.progressIndicators["\(scope) activity indicator for \"\(label)\""]
+        }
+        
+        var allElements: [String: XCUIElement] {
+            return ["headerText": headerLabel, "durationText": durationLabel, "disclosure": disclosure, "indicator": indicatorBar]
+        }
     }
     
     struct Ptn {