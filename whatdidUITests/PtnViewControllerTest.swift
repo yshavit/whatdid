d3b34c458e6b66aa902c045bb8602934607d8236
Yuval Shavit <yankee.sierra@gmail.com>
2020-09-01 06:39:44 -0400

    before/after screenshots when clicking disclosure

--- whatdidUITests/PtnViewControllerTest.swift
+++ whatdidUITests/PtnViewControllerTest.swift
@@ -534,16 +534,20 @@ class PtnViewControllerTest: XCTestCase {
         }
         
         func clickDisclosure(until element: XCUIElement, _ existence: Existence) {
-            disclosure.click()
-            wait(for: "element to \(existence.asVerb)") {
-                switch existence {
-                case .exists:
-                    return element.exists
-                case .isVisible:
-                    return element.isVisible
-                case .doesNotExist:
-                    return !element.exists
+            XCTContext.runActivity(named: "Click \(disclosure.simpleDescription)") {context in
+                context.add(XCTAttachment(screenshot: ancestor.screenshot()))
+                disclosure.click()
+                wait(for: "element to \(existence.asVerb)") {
+                    switch existence {
+                    case .exists:
+                        return element.exists
+                    case .isVisible:
+                        return element.isVisible
+                    case .doesNotExist:
+                        return !element.exists
+                    }
                 }
+                context.add(XCTAttachment(screenshot: ancestor.screenshot()))
             }
         }
     }
