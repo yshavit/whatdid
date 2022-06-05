ed0435d4a2183cea96d2bec96e6b6832a9031536
Yuval Shavit <yankee.sierra@gmail.com>
2022-06-05 01:00:53 -0400

    check for TODOs

--- whatdid/main/AutoCompletingField.swift
+++ whatdid/main/AutoCompletingField.swift
@@ -427,7 +427,7 @@ fileprivate class PopupManager: NSObject, NSWindowDelegate {
             return optionFields.map { $0.stringValue }
         }
         set (values) {
-            
+            #warning("todo hello")
             mainStack.views.forEach { $0.removeFromSuperview() }
             mainStack.subviews.forEach { $0.removeFromSuperview() }
             matchedSectionSeparators.removeAll()
