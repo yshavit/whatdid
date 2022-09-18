7ad8ad1c841936b49acf7eee1dd0ade27f44ec2b
Yuval Shavit <yankee.sierra@gmail.com>
2022-09-18 15:18:01 -0400

    stub ProjectTaskFinder

---
+++ whatdid/views/ProjectTaskFinder.swift
@@ -0,0 +1,21 @@
+// whatdid?
+
+import Cocoa
+
+class ProjectTaskFinder: WdView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+    @IBAction func onSelect(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func onCancel(_ sender: Any) {
+        
+    }
+}
