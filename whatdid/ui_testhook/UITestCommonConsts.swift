42695732b4d79f7cc860f680b0e2a6894d6c549f
Yuval Shavit <yankee.sierra@gmail.com>
2023-05-14 20:56:43 -0400

    fix clicking while there's a selection
    No unit tests, because I'm lazy. (testClickAtOtherThanSelection checks
    the TextOptionsList's handling of it, but that wasn't where the bug
    was.)
    
    Also some build schema stuff, to hide the PTN from popping up on
    startup.
    
    Fixes #321 (contact!)

--- whatdid/ui_testhook/UITestCommonConsts.swift
+++ whatdid/ui_testhook/UITestCommonConsts.swift
@@ -6,6 +6,7 @@ import Cocoa
 private let SUPPRESS_TUTORIAL_KEY = "SUPPRESS_TUTORIAL"
 private let SUPPRESS_TUTORIAL_VAL = "true"
 let SHOW_TUTORIAL_ON_FIRST_START = ProcessInfo.processInfo.environment[SUPPRESS_TUTORIAL_KEY] != SUPPRESS_TUTORIAL_VAL
+let SILENT_STARTUP = true
 
 func startupEnv(suppressTutorial: Bool) -> [String: String] {
     var env = [String: String]()
@@ -17,4 +18,5 @@ func startupEnv(suppressTutorial: Bool) -> [String: String] {
 
 #else
 let SHOW_TUTORIAL_ON_FIRST_START = true
+let SILENT_STARTUP = false
 #endif
