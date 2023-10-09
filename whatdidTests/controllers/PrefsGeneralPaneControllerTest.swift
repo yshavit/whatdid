8aee516139deb3e9b2bfdde2651e9d4c95e38b85
Yuval Shavit <yankee.sierra@gmail.com>
2023-10-09 16:05:11 -0400

    fix test

--- whatdidTests/controllers/PrefsGeneralPaneControllerTest.swift
+++ whatdidTests/controllers/PrefsGeneralPaneControllerTest.swift
@@ -3,13 +3,13 @@
 import XCTest
 @testable import whatdid
 
-class PrefsViewControllerTest: XCTestCase {
+class PrefsGeneralPaneControllerTest: XCTestCase {
 
     func testExportFormat() {
         /// Saturday, June 4, 2022 9:23:40 PM GMT-04:00 DST
         let date = Date(timeIntervalSince1970: 1654392220)
         let scheduler = DummyScheduler(now: date)
-        let actual = PrefsViewController.exportFileName(JsonEntryExportFormat(), scheduler)
+        let actual = PrefsGeneralPaneController.exportFileName(JsonEntryExportFormat(), scheduler)
         XCTAssertEqual("whatdid-export-2022-06-04T212340-0400.json", actual)
     }
     
